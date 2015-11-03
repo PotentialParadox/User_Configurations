8bf27f4e10cac8ed10ac4314750890a47d83fa7d
Dustin <dtracy@ufl.edu>
2015-11-02 22:40:20 -0500

    add full install

---
+++ full_install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cp ~/.vimrc ~/.vimrc.back
+cp ~/.tmux.conf ~/.tmux.conf
+
+cp -f ~/User_Configurations/{.vimrc,.tmux.conf} ~/
+
+if [ "$(uname)" == "Darwin" ]; then
+  if [ ! -f /usr/local/bin/brew ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
+  brew update
+  if [ ! -d /usr/local/Cellar/vim ]; then
+    brew install vim
+  fi
+  if [ ! -d /usr/local/Cellar/tmux ]; then
+    brew uninstall libevent
+    sudo chown -R $(whoami) /usr/local
+    brew install tmux
+  fi
+  if [ ! -d /usr/local/Cellar/zsh ]; then
+    brew install zsh
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sudo chsh -s $(which zsh) $(whoami)
+  fi
+fi
+
+dist=`grep DISTRIB_ID /etc/*-release | awk -F '=' '{print $2}'`
+
+if [ "$dist" == "Ubuntu" ]; then
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+  sudo apt-get install -y vim tmux curl zsh
+  if [ ! $SHELL = /usr/bin/zsh ]; then
+    echo "zsh installed"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sudo chsh -s $(which zsh) $(whoami)
+  fi
+  vim -c "PlugInstall"
+fi
+
+
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
