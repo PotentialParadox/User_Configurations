85c8892a52344e029e0533888e0cf6c087f005dd
PotentialParadox <dtracy@ufl.edu>
2015-11-11 21:50:10 -0500

    fixed vi-mode bug

--- zsh_config.py
+++ zsh_config.py
@@ -12,20 +12,8 @@ fin.close()
 
 p_theme = re.compile('ZSH_THEME\S+')
 new_file_string = re.sub(p_theme, 'ZSH_THEME="dustin"', file_string)
+p_vi = re.compile('plugins\=\(git\)')
+new_file_string1 = re.sub(p_vi, 'plugins=(git vi-mode)', new_file_string)
 
 fout = open(home + '/.zshrc', 'w')
-fout.write(new_file_string)
-fout.write(
-        'bindkey -v\n'
-        'export KEYTIMEOUT=1\n'
-        'bindkey \'^r\' history-incremental-pattern-search-backward\n'
-        'function zle-line-init zle-keymap-select {\n'
-            'RPS1="${${KEYMAP/vicmd/-- NORMAL --}/(main|viins)/-- INSERT --}"\n'
-            'RPS2=$RPS1\n'
-            'zle reset-prompt\n'
-        '}\n'
-        'zle -N zle-line-init\n'
-        'zle -N zle-keymap-select\n'
-           )
-fout.close()
-
+fout.write(new_file_string1)
