f721239269312e0c02ec197cad534930a066f5d1
Dustin Tracy <dtracy@ufl.edu>
2016-04-08 16:58:25 -0400

    Fixed theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -180,24 +180,10 @@
 
 (golden-ratio-mode)
 
-(load-theme 'solarized-light)
+(load-theme 'solarized-light t)
 
 ;; Remove tool-bar
 (tool-bar-mode -1)
 
 ;;; === End Theme ===
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
