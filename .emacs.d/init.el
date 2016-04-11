1a156d744d506d3bcf65331420e1213830e4266d
Dustin Tracy <dtracy@ufl.edu>
2016-04-11 17:33:03 -0400

    fixed flycheck error

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -139,7 +139,7 @@
 
 (require 'flycheck-irony)
 (eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+  '(add-hook 'flycheck-mode-hook 'flycheck-irony-setup))
 ;;; === End C++ Flycheck Configuration ===
 
 ;;; === Begin cmake-ide Configuration ===
